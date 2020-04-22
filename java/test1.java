c197a77d72dec70a803c77ad1d7c84522ee587a7
liaoyunfeng <liaoyunfeng@shein.com>
2020-04-22 10:08:53 +0800

    MLB-8033:feature:你好啊

---
+++ java/test1.java
@@ -0,0 +1,13 @@
+/**
+ * @Description TODO
+ * @ClassName test1@Description TODO
+ * @Author xiaofeng
+ * @copyright Copyright (C) 2019 SHEIN, Inc. All right reserved. <br>
+ * @company 南京领添科技有限公司
+ * @Date 2020/4/22 10:08
+ **/
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
